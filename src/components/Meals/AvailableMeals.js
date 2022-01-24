8d94ddbef17e62dd06c8cc8502739126a17008ee
Nanlung <nanlung.temlong@gmail.com>
2022-01-24 18:13:08 +0100

    loading feature

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,6 +5,7 @@ import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -24,10 +25,18 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    <section className={classes.MealsLoading}>
+      <p>Loading...</p>
+    </section>;
+  }
+
   const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
